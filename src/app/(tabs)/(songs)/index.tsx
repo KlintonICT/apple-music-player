cd852c023d7a10d53f44c3110a273809f6300a6a
KlintonICT <chhunklinton91@gmail.com>
2024-10-15 17:18:16 +0700

    feat: create search bar

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,13 +1,30 @@
 import { TracksList } from '@/components/TracksList';
 import { screenPadding } from '@/constants/tokens';
+import { useNavigationSearch } from '@/hooks/useNavigationSearch';
 import { defaultStyles } from '@/styles';
 import { ScrollView, View } from 'react-native';
 
+import library from '@/assets/data/library.json';
+import { trackTitleFilter } from '@/helpers/filter';
+import { useMemo } from 'react';
+
 const SongsScreen = () => {
+	const search = useNavigationSearch({
+		searchBarOptions: {
+			placeholder: 'Find in songs',
+		},
+	});
+
+	const filteredTracks = useMemo(() => {
+		if (!search) return library;
+
+		return library.filter(trackTitleFilter(search));
+	}, [search]);
+
 	return (
 		<View style={defaultStyles.container}>
 			<ScrollView contentInsetAdjustmentBehavior="automatic" style={{ paddingHorizontal: screenPadding.horizontal }}>
-				<TracksList scrollEnabled={false} />
+				<TracksList tracks={filteredTracks} scrollEnabled={false} />
 			</ScrollView>
 		</View>
 	);
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,18 +1,21 @@
-import library from '@/assets/data/library.json';
 import { utilsStyles } from '@/styles';
 import React from 'react';
 import { FlatList, FlatListProps, View } from 'react-native';
 import { TrackListItem } from './TrackListItem';
 
-export type TracksListProps = Partial<FlatListProps<unknown>>;
+export type TracksListProps = Partial<FlatListProps<unknown>> & {
+	tracks: any[];
+};
 
 const ItemDivider = () => <View style={{ ...utilsStyles.itemSeparator, marginVertical: 9, marginLeft: 60 }} />;
 
-export const TracksList = ({ ...flatListProps }: TracksListProps) => {
+export const TracksList = ({ tracks, ...flatListProps }: TracksListProps) => {
 	return (
 		<FlatList
-			data={library}
+			data={tracks}
+			contentContainerStyle={{ paddingTop: 10, paddingBottom: 128 }}
 			ItemSeparatorComponent={ItemDivider}
+			ListFooterComponent={ItemDivider}
 			renderItem={({ item }) => (
 				<TrackListItem
 					track={{
